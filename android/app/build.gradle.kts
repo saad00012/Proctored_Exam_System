5e4e988239ddeecdbef7a5afe80fea5e0c930604
saad00012 <saadsayyad0001@gmail.com>
2026-08-10 14:42:45 +0530

    Security: Remove sensitive files from tracking, update .gitignore

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -68,6 +68,10 @@ dependencies {
   implementation(libs.androidx.compose.ui.tooling.preview)
   implementation(libs.androidx.compose.material3)
   implementation("androidx.compose.material:material-icons-core")
+  implementation("androidx.compose.material:material-icons-extended")
+  implementation("androidx.compose.animation:animation")
+
+
   // Tooling
   debugImplementation(libs.androidx.compose.ui.tooling)
   // Instrumented tests
